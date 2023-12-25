ac3224e56e77e91dde5f3b19d22a16c40d3afab4
Wentai_Ye <yewentai126@gmail.com>
2023-12-25 16:59:22 +0100

    update

--- yewentai/mainonline_anf.c
+++ yewentai/mainonline_anf.c
@@ -1,42 +1,41 @@
-//#include <aic3204.h>
-//#include <dsplib.h>
-//#include <stdio.h>
-//#include <usbstk5515.h>
-//
-//#include "anf.h"
-//#include "anf_cascade.h"
-//
-//#define SAMPLES_PER_SECOND 8000
-//#define GAIN_IN_dB 10
-//
-//int main() {
-//  // declare variables
-//
-//  USBSTK5515_init(); // Initializing the Processor
-//  aic3204_init();    // Initializing the Audio Codec
-//
-//  while (1) {
-//    // Read from microphone
-//	aic3204_codec_read(left, right);
-//
-//	// Implementation goes here...
-//	set_sampling_frequency_and_gain(SAMPLES_PER_SECOND, GAIN_IN_dB);
-//
-//	  while (1) {
-//	    // Read from microphone
-//	    aic3204_codec_read(&left, &right);
-//
-//	    // Process the audio signal using ANF or any other algorithm
-//	    e_left = anf(left);
-//	    e_right = anf(right);
-//
-//	    // Write to line out
-//	    aic3204_codec_write(e_left, e_right);
-//	  }
-//
-//    // Write to line out
-//    aic3204_codec_write(e, e);
-//  }
-//
-//  return 0;
-//}
+#include <aic3204.h>
+#include <dsplib.h>
+#include <stdio.h>
+#include <usbstk5515.h>
+
+#include "anf.h"
+#include "anf_cascade.h"
+
+#define SAMPLES_PER_SECOND 8000
+#define GAIN_IN_dB 10
+
+int main()
+{
+    // Initialize variables
+    int left, right;             // Variables to store audio input
+    int e;                       // Variable to store filtered output
+    int x[6] = {0};              // Circular buffer for ANF
+    int a[2] = {0};              // Adaptive filter coefficients for ANF
+    int rho[2] = {0};            // Rho values for ANF
+    unsigned int index[2] = {0}; // Index for circular buffer
+
+    // Initialize the Processor and Audio Codec
+    USBSTK5515_init();
+    aic3204_init();
+    set_sampling_frequency_and_gain(SAMPLES_PER_SECOND, GAIN_IN_dB);
+
+    while (1)
+    {
+        // Read audio input from the microphone
+        aic3204_codec_read(&left, &right);
+
+        // Here you can process the audio signal using ANF or any other algorithm.
+        // For example, using the anf_cascade function:
+        e = anf_cascade(left, x, a, rho, index); // Process left channel
+
+        // Write the processed output to the line out
+        aic3204_codec_write(e, e); // Writing the same output to both left and right channels
+    }
+
+    return 0;
+}
