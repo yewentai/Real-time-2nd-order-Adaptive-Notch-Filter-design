ec9ad268f25ee2daa6c7ca19469f7d63d87eb82f
Wentai_Ye <yewentai126@gmail.com>
2023-12-25 16:51:12 +0100

    update

--- yewentai/anf.c
+++ yewentai/anf.c
@@ -1,85 +1,99 @@
 #include "anf.h"
 
+/**
+ * Implements an adaptive notch filter (ANF) for signal processing.
+ *
+ * @param y The newly captured sample (input signal).
+ * @param x Pointer to a buffer holding the last three samples (circular buffer).
+ * @param a Pointer to the adaptive filter coefficient.
+ * @param rho Array containing the current and asymptotic values of rho.
+ * @param index Pointer to the current index in the circular buffer x.
+ *
+ * @return The filtered output signal.
+ *
+ * The function processes the input signal using a recursive filtering algorithm.
+ * It adapts the filter coefficient based on the input and updates the circular buffer.
+ * The algorithm involves fixed-point arithmetic with careful attention to scaling
+ * to prevent overflow and preserve precision.
+ */
 int anf(int y, int *x, int *a, int *rho, unsigned int* index)
 {
     int a_i = *a;
-    /*
-     y in Q?: newly captured sample
-     X in Q?: X[3] databuffer - Hint: Reserve a sufficient number of integer bits such that summing numbers does not cause overflow
-     (Hence no shift is needed after summing numbers)
-     A in Q?: the adaptive filter coefficients
-     e in Q?: output signal
-     rho in Q?: rho[2] = rho(t), rho(inf) }
-     index points to t-1 sample (t current time index) in X -> circular buffer
-     */
+    int e, k; // e: Filter output, k: Index for circular buffer
+    long AC0, AC1; // AC0, AC1: Temporary variables for intermediate calculations
 
-    int e, k; //16
-    long AC0, AC1; //32
-
-    //add your own code here
+    // Processing starts here
     k = *index;
-    /*
-     * modify rho section
-     * rho[0] is rho(t), rho[1] = rho(+inf)
-     */
 
+    // Modify rho
+    // rho[0] is rho(t), rho[1] = rho(inf)
     AC0 = (long)lambda * rho[0];
-    AC0 += 0x00004000;
-    AC0 >>= 15;
+    AC0 += 0x00004000; // Rounding
+    AC0 >>= 15; // Right shift for normalization
     AC1 = (long)lambda2 * rho[1];
-    AC1 += 0x00004000;
-    AC1 >>= 15;
+    AC1 += 0x00004000; // Rounding
+    AC1 >>= 15; // Right shift for normalization
     rho[0] = (int)(AC0 + AC1);
 
 
-    AC0 = (long)(*rho) * x[k]; // 16q15 * 16q11 = 32q26
-    AC0 += 0x00000400;
-    AC0 >>= 11;
-    /**
-     * according to the matlab file, AC now swings between -3.+ and 3.+
-     */
-    AC0 = (long)a_i * (AC0 >> 2); // 32q13 * 16q13 = 32q26
-    AC0 += 0x00000400;
-    AC0 >>= 11;
-    AC1 = -(long)a_i * x[k]; // 32q13 * 32q11 = 32q24
-    AC1 += 0x00000100;
-    AC1 >>= 9;
+    // Calculate the first intermediate value using the current rho and input sample x
+    AC0 = (long)(*rho) * x[k]; // Multiply rho(t) with the current sample in x
+    AC0 += 0x00000400; // Add rounding offset
+    AC0 >>= 11; // Normalize the result to maintain fixed-point precision
 
+    // Update AC0 based on filter coefficient a_i and the modified sample value
+    AC0 = (long)a_i * (AC0 >> 2); // Multiply by adaptive filter coefficient
+    AC0 += 0x00000400; // Add rounding offset
+    AC0 >>= 11; // Normalize the result
+
+    // Calculate AC1, another intermediate value for the filtering process
+    AC1 = -(long)a_i * x[k]; // Multiply by negated filter coefficient
+    AC1 += 0x00000100; // Add rounding offset
+    AC1 >>= 9; // Normalize the result
+
+    // Update index k for circular buffer manipulation
     k = (k + 1) % 3;
 
-    AC1 += ((long)x[k] << 4); // 32q15 + 32q15
+    // Perform intermediate calculations involving buffer values and rho
+    AC1 += ((long)x[k] << 4); // Sum with next sample in the buffer
 
-    long rho_sq = (long)(*rho) * *rho;
-    rho_sq += 0x00004000;
-    rho_sq >>= 15;
-    long temp = ((long)x[k] * (short)rho_sq);
-    temp += 0x00000400;
-    temp >>= 11;
-    AC0 -= temp;
+    // Calculate square of rho and use it in further processing
+    long rho_sq = (long)(*rho) * *rho; // Square rho
+    rho_sq += 0x00004000; // Add rounding offset
+    rho_sq >>= 15; // Normalize
+    long temp = ((long)x[k] * (short)rho_sq); // Multiply buffer sample by rho squared
+    temp += 0x00000400; // Add rounding offset
+    temp >>= 11; // Normalize
+    AC0 -= temp; // Subtract from AC0 for final adjustment
 
+    // Update buffer with the newly processed sample
     k = (k + 1) % 3;
+    x[k] = (short)(((long)y + AC0) >> 4); // Update buffer with new value
 
-    x[k] = (short)(((long)y + AC0) >> 4); //v[i]
-    //x_debug(i)= x[index];
-    e = (int)(((long)x[k] << 4) + (long)AC1);
+    // Calculate final output e using buffer and intermediate value AC1
+    e = (int)(((long)x[k] << 4) + (long)AC1); // Combine buffer value with AC1 for output
 
+    // Update filter coefficient a_i based on the output e and buffer value
     k = (k + 1) % 3;
-
-    AC0 = (long)mu * x[k]; // 16q15*16q11 = 32q26
-    AC0 += 0x00000400;
-    AC0 >>= 11; //32q15
-    AC0 = (long)e * (short)(AC0); // 16q15 * 16q15 = 32q30
-    AC0 += 0x00004000;
-    AC0 >>= 15; //15 + 2
-    a_i += (short)((AC0 + 0x2) >> 2);
+    AC0 = (long)mu * x[k]; // Multiply mu with current buffer sample
+    AC0 += 0x00000400; // Add rounding offset
+    AC0 >>= 11; // Normalize
+    AC0 = (long)e * (short)(AC0); // Multiply by output signal
+    AC0 += 0x00004000; // Add rounding offset
+    AC0 >>= 15; // Normalize
+    a_i += (short)((AC0 + 0x2) >> 2); // Update a_i
     k = (k + 2) % 3;
-    // necessary check to see if |a| < 2
+
+    // Check and limit the range of a_i to prevent overflow
     if (a_i < -0x4000)
         a_i = -0x4000;
     if (a_i > 0x4000)
         a_i = 0x4000;
     *a = a_i;
     
+    // Update the index for the next call
     *index = k;
+
+    // Return the filtered output
     return e;
 }
--- yewentai/anf_cascade.c
+++ yewentai/anf_cascade.c
@@ -1,125 +1,143 @@
 #include "anf.h"
 #include "anf_cascade.h"
 
+/**
+ * Implements a cascaded adaptive notch filter (ANF).
+ *
+ * @param y The newly captured sample (input signal).
+ * @param x Pointer to a buffer holding the last six samples (two sets of three for each cascade stage).
+ * @param a Array of two adaptive filter coefficients for each stage of the cascade.
+ * @param rho Array containing the current and asymptotic values of rho.
+ * @param index Array of two indices, each pointing to the current index in the circular buffer x for each stage.
+ *
+ * @return The output signal after processing through the first stage of the cascade.
+ *
+ * This function processes the input signal through two stages of ANF. Each stage adapts its filter
+ * coefficient based on the input and updates its part of the circular buffer. The output of the first stage
+ * is used as the input to the second stage. The function returns the output of the first stage.
+ */
 int anf_cascade(int y, int *x, int *a, int *rho, unsigned int* index)
 {
-    int a1 = a[0], a2 = a[1];
-    int e1, e2, k1, k2; //16
-    long AC0, AC1; //32
+    int a1 = a[0], a2 = a[1]; // Filter coefficients for the two stages
+    int e1, e2, k1, k2; // Output signals and indices for circular buffers
+    long AC0, AC1; // Temporary variables for intermediate calculations
 
-    //add your own code here
+    // Processing for the first stage of the cascade
     k1 = index[0];
 
+    // Update rho based on lambda and lambda2 constants
+    // This modification affects the response of the filter
     AC0 = (long)lambda * rho[0];
-    AC0 += 0x00004000;
-    AC0 >>= 15;
+    AC0 += 0x00004000; // Rounding constant for fixed-point arithmetic
+    AC0 >>= 15; // Bit shift for normalization
     AC1 = (long)lambda2 * rho[1];
-    AC1 += 0x00004000;
-    AC1 >>= 15;
+    AC1 += 0x00004000; // Rounding constant
+    AC1 >>= 15; // Bit shift for normalization
     rho[0] = (int)(AC0 + AC1);
 
+    // Process the input sample y and update the circular buffer
+    AC0 = (long)(rho[0]) * x[k1]; // Perform fixed-point multiplication
+    AC0 += 0x00000400; // Rounding constant
+    AC0 >>= 11; // Bit shift for normalization
+    AC0 = (long)a1 * (AC0 >> 2); // Apply filter coefficient a1
+    AC0 += 0x00002000; // Rounding constant
+    AC0 >>= 11; // Bit shift for normalization
+    AC1 = -(long)a1 * x[k1]; // Negate and multiply by filter coefficient
+    AC1 += 0x00000100; // Rounding constant
+    AC1 >>= 9; // Bit shift for normalization
 
-    AC0 = (long)(rho[0]) * x[k1]; // 16q15 * 16q11 = 32q26
-    AC0 += 0x00000400;
-    AC0 >>= 11;
-    /**
-     * according to the matlab file, AC now swings between -3.+ and 3.+
-     */
-    AC0 = (long)a1 * (AC0 >> 2); // 32q13 * 16q13 = 32q26
-    AC0 += 0x00002000;
-    AC0 >>= 11;
-    AC1 = -(long)a1 * x[k1]; // 32q13 * 32q11 = 32q24
-    AC1 += 0x00000100;
-    AC1 >>= 9;
-
+    // Update circular buffer index for first stage
     k1 = (k1 + 1) % 3;
 
-    AC1 += ((long)x[k1] << 4); // 32q15 + 32q15
+    // Additional processing steps using updated buffer value
+    AC1 += ((long)x[k1] << 4); // Left shift to adjust fixed-point precision
+    long rho_sq = (long)(rho[0]) * rho[0]; // Square rho value
+    rho_sq += 0x00004000; // Rounding constant
+    rho_sq >>= 15; // Bit shift for normalization
+    long temp = ((long)x[k1] * (short)rho_sq); // Multiply buffer value by squared rho
+    temp += 0x00000400; // Rounding constant
+    temp >>= 11; // Bit shift for normalization
+    AC0 -= temp; // Subtract to obtain new buffer value
 
-    long rho_sq = (long)(rho[0]) * rho[0];
-    rho_sq += 0x00004000;
-    rho_sq >>= 15;
-    long temp = ((long)x[k1] * (short)rho_sq);
-    temp += 0x00000400;
-    temp >>= 11;
-    AC0 -= temp;
-
+    // Update the circular buffer with new value
     k1 = (k1 + 1) % 3;
+    x[k1] = (short)(((long)y + AC0) >> 4); // Update buffer with new processed value
 
-    x[k1] = (short)(((long)y + AC0) >> 4); //v[i]
-    //x_debug(i)= x[index];
-    e1 = (int)(((long)x[k1] << 4) + (long)AC1);
+    // Calculate the output of the first stage
+    e1 = (int)(((long)x[k1] << 4) + (long)AC1); // Combine buffer value with AC1
 
+    // Continue processing for updating the filter coefficient
     k1 = (k1 + 1) % 3;
-
-    AC0 = (long)mu * x[k1]; // 16q15*16q11 = 32q26
-    AC0 += 0x00000400;
-    AC0 >>= 11; //32q15
-    AC0 = (long)e1 * (short)(AC0); // 16q15 * 16q15 = 32q30
-    AC0 += 0x00004000;
-    AC0 >>= 15; //15 + 2
-    a1 += (short)((AC0 + 0x2) >> 2);
-    k1 = (k1 + 2) % 3;
-    //not necessary
+    AC0 = (long)mu * x[k1]; // Apply mu constant
+    AC0 += 0x00000400; // Rounding constant
+    AC0 >>= 11; // Bit shift for normalization
+    AC0 = (long)e1 * (short)(AC0); // Multiply by output signal
+    AC0 += 0x00004000; // Rounding constant
+    AC0 >>= 15; // Bit shift for normalization
+    // Update the adaptive filter coefficient for the first stage
+    a1 += (short)((AC0 + 0x2) >> 2); // Adjust the filter coefficient
+    // Ensure the coefficient stays within a specific range to avoid overflow
     if (a1 < -0x4000)
         a1 = -0x4000;
     if (a1 > 0x4000)
         a1 = 0x4000;
-    a[0] = a1;
-    index[0] = k1;
-    
-    //cascade part starts here
-    //e is now our y
+    a[0] = a1; // Store the updated coefficient
+    index[0] = k1; // Update the buffer index for the first stage
+
+    // Begin processing for the second stage of the cascade
+    // Use the output of the first stage (e1) as the input to the second stage
     k2 = index[1];
 
+    // Process the signal similar to the first stage, but using second stage parameters
+    // This involves similar steps of multiplication, rounding, and normalization
     AC0 = (long)(rho[0]) * x[k2 + 3]; // 16q15 * 16q11 = 32q26
     AC0 += 0x00000400;
     AC0 >>= 11;
-    /**
-     * according to the matlab file, AC now swings between -3.+ and 3.+
-     */
-    AC0 = (long)a2 * (AC0 >> 2); // 32q13 * 16q13 = 32q26
+    AC0 = (long)a2 * (AC0 >> 2); // Apply second filter coefficient a2
     AC0 += 0x00000400;
     AC0 >>= 11;
-    AC1 = -(long)a2 * x[k2 + 3]; // 32q13 * 32q11 = 32q24
+    AC1 = -(long)a2 * x[k2 + 3]; // Negate and apply a2
     AC1 += 0x00000100;
     AC1 >>= 9;
 
+    // Update the circular buffer index for the second stage
     k2 = (k2 + 1) % 3;
 
-    AC1 += ((long)x[k2 + 3] << 4); // 32q15 + 32q15
-
-    rho_sq = (long)(rho[0]) * rho[0];
+    // Additional processing for the second stage
+    AC1 += ((long)x[k2 + 3] << 4); // Adjust fixed-point precision
+    rho_sq = (long)(rho[0]) * rho[0]; // Square rho value for the second stage
     rho_sq += 0x00004000;
     rho_sq >>= 15;
-    temp = ((long)x[k2 + 3] * (short)rho_sq);
+    temp = ((long)x[k2 + 3] * (short)rho_sq); // Multiply buffer value by squared rho
     temp += 0x00000400;
     temp >>= 11;
-    AC0 -= temp;
+    AC0 -= temp; // Obtain new buffer value for the second stage
 
+    // Update the circular buffer with the new value for the second stage
     k2 = (k2 + 1) % 3;
+    x[k2 + 3] = (short)(((long)e1 + AC0) >> 4); // Update buffer with the processed value
 
-    x[k2 + 3] = (short)(((long)e1 + AC0) >> 4); //v[i]
-    //x_debug(i)= x[index];
+    // Calculate the output of the second stage
     e2 = (int)(((long)x[k2 + 3] << 4) + (long)AC1);
 
+    // Update the filter coefficient for the second stage
     k2 = (k2 + 1) % 3;
-
-    AC0 = (long)mu * x[k2 + 3]; // 16q15*16q11 = 32q26
+    AC0 = (long)mu * x[k2 + 3];
     AC0 += 0x00000400;
-    AC0 >>= 11; //32q15
-    AC0 = (long)e2 * (short)(AC0); // 16q15 * 16q15 = 32q30
+    AC0 >>= 11;
+    AC0 = (long)e2 * (short)(AC0);
     AC0 += 0x00004000;
-    AC0 >>= 15; //15 + 2
+    AC0 >>= 15;
     a2 += (short)((AC0 + 0x2) >> 2);
-    k2 = (k2 + 2) % 3;
-    //not necessary
+
+    // Ensure the coefficient stays within a specified range
     if (a2 < -0x4000)
         a2 = -0x4000;
     if (a2 > 0x4000)
         a2 = 0x4000;
-    a[1] = a2;
-    index[1] = k2;
+    a[1] = a2; // Store the updated coefficient
+    index[1] = k2; // Update the buffer index for the second stage
 
+    // Return the output of the first stage
     return e1;
 }
