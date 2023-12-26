028245570d8f82d3f4819a2dcf9ba290625732cb
Wentai_Ye <yewentai126@gmail.com>
2023-12-26 15:46:28 +0100

    update

--- anf.c
+++ anf.c
@@ -1,99 +1,102 @@
 #include "anf.h"
 
-/**
- * Implements an adaptive notch filter (ANF) for signal processing.
- *
- * @param y The newly captured sample (input signal).
- * @param x Pointer to a buffer holding the last three samples (circular buffer).
- * @param a Pointer to the adaptive filter coefficient.
- * @param rho Array containing the current and asymptotic values of rho.
- * @param index Pointer to the current index in the circular buffer x.
- *
- * @return The filtered output signal.
- *
- * The function processes the input signal using a recursive filtering algorithm.
- * It adapts the filter coefficient based on the input and updates the circular buffer.
- * The algorithm involves fixed-point arithmetic with careful attention to scaling
- * to prevent overflow and preserve precision.
- */
-int anf(int y, int *x, int *a, int *rho, unsigned int* index)
+int anf(int y, int *s, int *a, unsigned int* rho, unsigned int* index)
 {
-    int a_i = *a;
-    int e, k; // e: Filter output, k: Index for circular buffer
-    long AC0, AC1; // AC0, AC1: Temporary variables for intermediate calculations
+    /*
+     y in Q : newly captured sample
+     s in Q : x[3] databuffer - Hint: Reserve a sufficiently number of integer bits such that summing intermediate values does not cause overflow (so no shift is needed after summing numbers)
+     a in  : the adaptive coefficient
+     e in Q : output signal
+     rho in Q : variable {rho, rho_inf} pole radius
+     index : points to (t-1) sample (t current time index) in s -> circular buffer
+     */
 
-    // Processing starts here
-    k = *index;
+    int e; // Error signal
+    long AC0, AC1; // Temporay 32 bit accumulator variables to avoid overflow
+    // Update the buffer index for the circular buffer implementation
+    int k = *index; // k is now pointing in fact ot m -1
+    /** ANF-LMS Algorithm implementation */ 
+    /** 
+     * Step 1: Update rho(m) = lambda * rho(m - 1) + one_minus_lambda * rho(inf)
+     * modify rho section
+     * rho[0] is rho(m-1), rho[1] = rho(+infinite)
+     * compute the latest rho[m] using rho[0] since rho[m-1] is no longer needed
+     */
+    
+    AC0 = (long)lambda * rho[0]; /* multiplication with long to reserve enough bits avoid overflow => 16q15 * 16q16 = 32q31 */
+    /** no need adding 0x00004000 since it's desired to maintain all the fractional bits without rounding, as we're representing a number in the range 0 to 1 with no integer part.*/
+    // AC0 += 0x00004000;  /** Round the part we'll be truncating (Carry bit of this sum will be the LSB after conversion)*/
+    AC0 >>= RHO_Q_FORMAT; /** Shift q-foramt bits right to normalize fix-point => Now AC0 is 32q15 */
+    AC1 = (long)one_minus_lambda * rho[1]; /* multiplication with long to reserve enough bits avoid overflow => 16q15 * 16q16 = 32q31 */
+    // AC1 += 0x00004000;  /** Round the part we'll be truncating (Carry bit of this sum will be the LSB after conversion)*/
+    AC1 >>= RHO_Q_FORMAT; /** Shift q-format bits right to fix-point => Now AC1 is 32q15 */
+    rho[0] = (unsigned int) ((AC0 + AC1) >> 1); /** update rho[m] => Convert back to 16q16*/
+ 
+    /** 
+     * Step 2: Update s(m) = y(m) + rho(m)a(m - 1)s(m - 1) - rho(m)^2 s(m - 2)
+     * modify s section 
+     * compute rho[0] square
+     * compute terms for s[m]*/ 
+    // Calculate rho squared in 32q16 format to avoid overflow
+    long rho_square = (long)rho[0] * rho[0]; // 16q16 * 16q16 = 32q32
+    rho_square >>= RHO_Q_FORMAT; // Now rho_square is 32q16
 
-    // Modify rho
-    // rho[0] is rho(t), rho[1] = rho(inf)
-    AC0 = (long)lambda * rho[0];
-    AC0 += 0x00004000; // Rounding
-    AC0 >>= 15; // Right shift for normalization
-    AC1 = (long)lambda2 * rho[1];
-    AC1 += 0x00004000; // Rounding
-    AC1 >>= 15; // Right shift for normalization
-    rho[0] = (int)(AC0 + AC1);
+    // Calculate the first term of s[m]: rho(m) * a(m - 1) * s(m - 1) with k = (m - 1)
+    AC0 = (long)rho[0] * a[k]; // 16q16 * 16q15 = 32q31
+    AC0 += 0x4000; // // Add half (since we are shifting 15 bits) to round
+    AC0 >>= A_Q_FORMAT; // Now AC0 is 32q16 to match with rho_square
+    AC0 *= s[k]; // 32q16 * 16q15 (assuming s is in q15 format) = 32q31
+    AC0 += 0x4000; // Add half to round
+    AC0 >>= A_Q_FORMAT; // Normalize to 32q16
 
+    // Calculate the a(m - 1)s(m - 1) term in e computation at current instance
+    long AC2 = -(long)a[k] * s[k]; // 16q15 * 16q15 = 32q30
+    AC2 += 0x4000;
+    AC2 >>= A_Q_FORMAT; // Normalize to 32q15
 
-    // Calculate the first intermediate value using the current rho and input sample x
-    AC0 = (long)(*rho) * x[k]; // Multiply rho(t) with the current sample in x
-    AC0 += 0x00000400; // Add rounding offset
-    AC0 >>= 11; // Normalize the result to maintain fixed-point precision
+    k = (k + 1) % 3; // circulat buffer => k++ means one sample stored earlier k++ => (m - 2)
 
-    // Update AC0 based on filter coefficient a_i and the modified sample value
-    AC0 = (long)a_i * (AC0 >> 2); // Multiply by adaptive filter coefficient
-    AC0 += 0x00000400; // Add rounding offset
-    AC0 >>= 11; // Normalize the result
+    // Calculate the second term of s[m]: rho(m)^2 * s(m - 2)
+    AC1 = rho_square * s[k]; // 32q16 * 16q15 = 32q31
+    AC1 += 0x4000; // Add half to round
+    AC1 >>= A_Q_FORMAT; // Normalize to 32q16
 
-    // Calculate AC1, another intermediate value for the filtering process
-    AC1 = -(long)a_i * x[k]; // Multiply by negated filter coefficient
-    AC1 += 0x00000100; // Add rounding offset
-    AC1 >>= 9; // Normalize the result
+    // Integrate the s(m-2) to the -(long)a(m-1) * s(m-1)
+    e = (short)((long)s[k] + (long)AC2 ); // 16q15 + 16q15  
 
-    // Update index k for circular buffer manipulation
+    k = (k + 1) % 3; // circulat buffer => k++ means one sample earlier k++ => m - 3 => m
+
+    // Combine terms and update s[m], ensuring all values are in 16q15 format before addition
+    s[k] = (short)((((long)y << 1) + AC0 - AC1) >> 16); // Convert y to 32q16, add/subtract, convert result back to 16q15
+
+    /** 
+     * Step 3: Update error e(m) = s(m) - a(m - 1)s(m - 1) + s(m - 2)
+     * modify e section 
+     * compute terms for s[m]
+     * */ 
+    e += s[k];
+
+    // Step 3: Update a[m]
+    // Assuming mu is in 16q15 format
+    AC0 = (long)mu * e; // 16q15 * 16q15 = 32q30
+    AC0 += 0x4000; // half for rounding, correct?
+    AC0 >>= A_Q_FORMAT; // 32q15
+    // Aadvance k to get to previous sample (from m to m -1 )
     k = (k + 1) % 3;
-
-    // Perform intermediate calculations involving buffer values and rho
-    AC1 += ((long)x[k] << 4); // Sum with next sample in the buffer
-
-    // Calculate square of rho and use it in further processing
-    long rho_sq = (long)(*rho) * *rho; // Square rho
-    rho_sq += 0x00004000; // Add rounding offset
-    rho_sq >>= 15; // Normalize
-    long temp = ((long)x[k] * (short)rho_sq); // Multiply buffer sample by rho squared
-    temp += 0x00000400; // Add rounding offset
-    temp >>= 11; // Normalize
-    AC0 -= temp; // Subtract from AC0 for final adjustment
-
-    // Update buffer with the newly processed sample
-    k = (k + 1) % 3;
-    x[k] = (short)(((long)y + AC0) >> 4); // Update buffer with new value
-
-    // Calculate final output e using buffer and intermediate value AC1
-    e = (int)(((long)x[k] << 4) + (long)AC1); // Combine buffer value with AC1 for output
-
-    // Update filter coefficient a_i based on the output e and buffer value
-    k = (k + 1) % 3;
-    AC0 = (long)mu * x[k]; // Multiply mu with current buffer sample
-    AC0 += 0x00000400; // Add rounding offset
-    AC0 >>= 11; // Normalize
-    AC0 = (long)e * (short)(AC0); // Multiply by output signal
-    AC0 += 0x00004000; // Add rounding offset
-    AC0 >>= 15; // Normalize
-    a_i += (short)((AC0 + 0x2) >> 2); // Update a_i
+    AC0 = ((long)AC0 * s[k]); // 32q15 * 16q15 = 32q30
+    AC0 += 0x4000; // half for rounding
+    a[k] = (short)(a[k] + (AC0 >> A_Q_FORMAT)); // Convert result back to 16q15
+    
+    // Advance k back to m
     k = (k + 2) % 3;
 
-    // Check and limit the range of a_i to prevent overflow
-    if (a_i < -0x4000)
-        a_i = -0x4000;
-    if (a_i > 0x4000)
-        a_i = 0x4000;
-    *a = a_i;
-    
-    // Update the index for the next call
-    *index = k;
+    // necessary check to see if |a| < 2
+    if (*a < -0x4000)
+        *a = -0x4000;
+    if (*a > 0x4000)
+        *a= 0x4000;
 
-    // Return the filtered output
-    return e;
+    *index = k; // Update the circular buffer index
+
+    return e; // Return the error signal
 }
