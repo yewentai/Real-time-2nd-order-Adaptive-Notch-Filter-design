0cda84c47d1935539a6b2b49f2c4f80fc75c1b81
yewentai <yewentai126@gmail.com>
2023-12-29 10:13:02 +0100

    update

---
+++ deliverables/anf.h
@@ -0,0 +1,32 @@
+#ifndef ANF_H
+#define ANF_H
+
+/**
+ * 0 < rho < 1 => unsigned 16q16 fixed-point representation from 0 up to (2^16 - 1)/(2^16), just under 1. The resolution would be 1/(2^16), which is approximately 0.0000153.
+ * ANF-LMS coefficient a => -1 < a < 1 => signed 16q15 => -1 < var < 1
+ * To ensure filter stability, a(t) must be clamped within the range -1 to 1 after each update.
+ * int => to store both positive and negative
+ * unsigned int => positive only
+ */
+// Define the fixed-point format for rho (0q16 in this case)
+#define RHO_Q_FORMAT 16
+
+// Define the fixed-point format for a (16q15 in this case)
+#define A_Q_FORMAT 15
+#define RHO_Q_FORMAT    16
+#define L_Q_FORMAT  15
+#define S_Q_FORMAT  11
+#define A_Q_FORMAT  15
+#define E_Q_FORMAT  15
+// Define the step size, mu, properly scaled for the fixed-point format
+// mu is always < 1, hence using 0q16 format
+#define mu 0xC8 // 16q15, approximately 200 in decimal
+
+// Define lambda and one_minus_lambda for 0q16 format
+// Both are always <= 1, hence using 16q15 format
+#define lambda 0x4CCC //0x7333 // Approximately 0.9 in 16q15 format
+#define one_minus_lambda 0x3333 //0x0CCD // Approximately 0.1 in 16q15 format
+
+int anf(short y, short *s , unsigned short *a, unsigned short *rho, int* index);
+
+#endif
---
+++ deliverables/main_online_anf.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <usbstk5515.h>
+#include "usbstk5515_i2c.h"
+#include <stdlib.h>
+#include <aic3204.h>
+#include <dsplib.h>
+
+#include "anf.h"
+#define SAMPLES_PER_SECOND 8000
+#define GAIN_IN_dB 10
+
+int main()
+{
+  // declare variables
+  int s[3] = {0, 0, 0}; // State vector X
+  int a[2] = {0x2000, 0x2000};
+  unsigned int rho[2] = {0x6666, 0x6666}; // Initial values for rho(t), rho(Inf)
+  int y, e;                               // input sample y, error signal sample e
+  unsigned int index = 0;
+  Int16 left, right; // Variables to store mic two channels data
+  char temp[2];
+
+  USBSTK5515_init(); // Initializing the Processor
+  aic3204_init();    // Initializing the Audio Codec
+  set_sampling_frequency_and_gain(SAMPLES_PER_SECOND, GAIN_IN_dB);
+
+  FILE *fpOut = fopen("..\\data\\outpureal_codec.pcm", "wb");
+  if (fpOut == NULL)
+  {
+    printf("Can't open output file. Exiting.\n");
+    return 1;
+  }
+
+  // while (1) {
+  //   // Read from microphone
+  //   aic3204_codec_read(&left, &right);
+
+  //   // Implementation goes here...
+  //   y = left;  // Assuming you want to process the left channel
+  //   // For example, using the anf_cascade function:
+  //   e = anf(&y, &s, &a, &rho, &index); // Process left channel
+  //   // Write to output file for evaluation
+  //   // Convert 16-bit integer back to 2 bytes in little-endian format
+  //   temp[0] = (short) (e & 0x00FF);
+  //   temp[1] = (short) (e & 0xFF00) >> 8;
+  //   fwrite(temp, sizeof(char), 2, fpOut);
+  //   // Write to line out
+  //   aic3204_codec_write(e, e);
+  // }
+
+  /** the following code is for testing only
+   * mic iniput directly to output. it shows that there was no real stereo input
+   * given that both with filter anf and without filter ( mic read to mic write) are both flat in spectrum
+   */
+
+  int seconds = 10;                                // Duration for which you want to run the loop
+  int totalSamples = SAMPLES_PER_SECOND * seconds; // Total samples to process
+
+  for (int i = 0; i < totalSamples; i++)
+  {
+    // Read from microphone
+    aic3204_codec_read(&left, &right);
+
+    // Implementation goes here...
+    y = left; // Assuming you want to process the left channel
+    // Comment out anf function to debug if microphone is working properly
+    e = anf(y, &s[0], &a[0], &rho[0], &index); // Process left channel
+    // Write to output file for evaluation
+    // Convert 16-bit integer back to 2 bytes in little-endian format
+    temp[0] = (short)(e & 0x00FF);
+    temp[1] = (short)(e & 0xFF00) >> 8;
+    fwrite(temp, sizeof(char), 2, fpOut);
+    // Write to line out
+    aic3204_codec_write(e, e);
+  }
+  fclose(fpOut);
+  return 0;
+}
